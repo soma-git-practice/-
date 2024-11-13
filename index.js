037720787a66459ff918a014e90f8d783047fc86
soma <somofgit@gmail.com>
2024-11-13 09:16:26 +0900

    途中まで

--- index.js
+++ index.js
@@ -5,5 +5,18 @@ const display = document.querySelector('.display');
 // ボタンに反応させる
 const buttons = document.querySelectorAll('.button');
 for (const btn of buttons) {
-  btn.addEventListener('click', e => console.log(e.target.innerHTML))
+  btn.addEventListener('click', e => {    
+    console.log(e.target.innerHTML)
+  })
 }
+
+// クリックしたものを１の位に表示する
+
+// display.childrenを順番に見ていき０以外が登場したら以降に.isActiveを追加する。
+// 一度も登場しなかった場合一番後ろに.isActiveを追加する。
+const displayItems = display.children;
+
+[...displayItems].reduce((result, current) => {
+  result.push(current);
+  return result;
+}, [])
