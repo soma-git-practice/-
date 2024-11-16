2808bd7b0bf215488681906369143cb0387f237f
soma <somofgit@gmail.com>
2024-11-16 19:50:21 +0900

    最新

--- index.js
+++ index.js
@@ -35,15 +35,63 @@ const DisplayOperate = function (array) {
   DisplayBlink();
 }
 
-// ボタンに反応させて配列に追加する
-const display_items = [];
+let display_items = [];
 const buttons = document.querySelectorAll('.button');
+let result = null;
 
-for (const btn of buttons) {
+// 1 ~ 9 をクリック時
+for (const btn of document.querySelectorAll('.button[data-type="number"]')) {
   btn.addEventListener('click', function () {
-
     display_items.push(this.textContent);
     DisplayOperate(display_items);
-
   })
 }
+
+// 0 をクリック時
+document.querySelector('.button[data-type="single_zero"]').addEventListener('click', function () {
+  if (display_items.length == 0) return false;
+  display_items.push(this.textContent);
+  DisplayOperate(display_items);
+})
+
+// 00 をクリック時
+document.querySelector('.button[data-type="double_zelo"]').addEventListener('click', function () {
+  if (display_items.length == 0) return false;
+  display_items = display_items.concat([0, 0]);
+  DisplayOperate(display_items);
+})
+
+// CCをクリック
+document.querySelector('.button[data-type="double_clear"]').addEventListener('click', function () {
+  display_items = [];
+  DisplayOperate(display_items);
+});
+
+// Cをクリック
+document.querySelector('.button[data-type="single_clear"]').addEventListener('click', function () {
+  display_items.pop();
+  DisplayOperate(display_items);
+});
+
+// +をクリック
+document.querySelector('.button[data-type="addition"]').addEventListener('click', function () {
+  let num = display_items.reduce((accumulator, currentValue) => accumulator += currentValue)
+  num = Number(num);
+  const sahen = (result === null) ? num : result(num)
+  // TODO 画面の数字消す
+  result = function (arg) {
+    return sahen + arg;
+  }
+});
+
+// =をクリック
+document.querySelector('.button[data-type="equal"]').addEventListener('click', function () {
+  let num = display_items.reduce((accumulator, currentValue) => accumulator += currentValue)
+  num = Number(num);
+  if (result === null) return false;
+  const sahen = result(num)
+  debugger
+
+  // TODO 数字を一文字ずつの配列にする。
+  result = null
+});
